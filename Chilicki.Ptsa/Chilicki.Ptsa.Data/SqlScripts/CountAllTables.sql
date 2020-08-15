03ce76135cd2d49369fce48c82a8c5c7beeec785
mchilicki <marcin.chilicki96@gmail.com>
2020-08-15 21:43:46 +0200

    Add CountAllTables script

---
+++ Chilicki.Ptsa/Chilicki.Ptsa.Data/SqlScripts/CountAllTables.sql
@@ -0,0 +1,27 @@
+
+SELECT count(*) as "AgenciesCount"
+  FROM [LahtiPtsaDb].[dbo].[Agencies]
+
+SELECT count(*) as "RoutesCount"
+  FROM [LahtiPtsaDb].[dbo].[Routes]
+
+SELECT count(*) as "StopsCount"
+  FROM [LahtiPtsaDb].[dbo].[Stops]
+
+SELECT count(*) as "TripsCount"
+  FROM [LahtiPtsaDb].[dbo].[Trips]
+
+SELECT count(*) as "StopTimesCount"
+  FROM [LahtiPtsaDb].[dbo].[StopTimes]
+
+SELECT count(*) as "GraphsCount"
+  FROM [LahtiPtsaDb].[dbo].[Graphs]
+
+SELECT count(*) as "VerticesCount"
+  FROM [LahtiPtsaDb].[dbo].[Vertices]
+
+SELECT count(*) as "ConnectionsCount"
+  FROM [LahtiPtsaDb].[dbo].[Connections]
+
+SELECT count(*) as "SimilarVerticesCount"
+  FROM [LahtiPtsaDb].[dbo].[SimilarVertices]
