43e44e71ba3f966e1a4390925012bb087005d9df
mchilicki <marcin.chilicki96@gmail.com>
2020-08-21 00:26:08 +0200

    Correct benchmark parameters to lower count

--- Chilicki.Ptsa/Chilicki.Ptsa.Search/Benchmarks/SingleCriteriaDijkstraVsMultipleCriterionDijkstra.cs
+++ Chilicki.Ptsa/Chilicki.Ptsa.Search/Benchmarks/SingleCriteriaDijkstraVsMultipleCriterionDijkstra.cs
@@ -2,7 +2,6 @@
 using BenchmarkDotNet.Engines;
 using Chilicki.Ptsa.Data.Entities;
 using Chilicki.Ptsa.Data.Repositories;
-using Chilicki.Ptsa.Domain.InputSearches;
 using Chilicki.Ptsa.Domain.Search.Aggregates;
 using Chilicki.Ptsa.Domain.Search.Aggregates.MultipleCriterion;
 using Chilicki.Ptsa.Domain.Search.Configurations.Options;
@@ -36,12 +35,6 @@ namespace Chilicki.Ptsa.Benchmarks
         public Graph Graph { get; set; }
         public IEnumerable<SearchInputDto> Searches { get; set; } = CurrentInputSearches.Searches;
 
-        [Params(30, 60, 120, 180, 240, 360, 480, 720)]
-        public int MaxTimeAheadFetchingPossibleConnections { get; set; }
-
-        [Params(3, 15, 30, 50)]
-        public int MinimumPossibleConnectionsFetched { get; set; }
-
         public SingleCriteriaDijkstraVsMultipleCriterionDijkstra()
         {
             Console.WriteLine("Constructor - Started");
@@ -60,16 +53,19 @@ namespace Chilicki.Ptsa.Benchmarks
             Graph = await GraphSingleton.GetGraph(graphRepository);
         }
 
-        //[Benchmark]
-        //[Arguments(0, 8)]
-        //[Arguments(0, 6)]
-        //[Arguments(0, 4)]
-        //[Arguments(4, 8)]
-        //[Arguments(4, 6)]
-        public void SingleCriteriaDijkstra(int minimalTransferTime, int maximalTransferDistanceInMinutes)
+        [Benchmark]
+        [Arguments(0, 8)]
+        [Arguments(0, 6)]
+        [Arguments(0, 4)]
+        [Arguments(4, 8)]
+        [Arguments(4, 6)]
+        public void SingleCriteriaDijkstra(
+            int minimalTransferTime,
+            int maximalTransferDistanceInMinutes)
         {
             var list = new List<FastestPath>();
-            var parameters = CreateParameters(minimalTransferTime, maximalTransferDistanceInMinutes);
+            var parameters = CreateParameters(
+                120, 15, minimalTransferTime, maximalTransferDistanceInMinutes);
             foreach (var search in Searches)
             {
                 var searchInput = searchInputMapper.ToDomainFromGraph(search, parameters, Graph);
@@ -81,15 +77,74 @@ namespace Chilicki.Ptsa.Benchmarks
         }
 
         [Benchmark]
-        [Arguments(0, 8)]
-        [Arguments(0, 6)]
-        [Arguments(0, 4)]
-        [Arguments(4, 8)]
-        [Arguments(4, 6)]
-        public void MultipleCriteriaDijkstra(int minimalTransferTime, int maximalTransferDistanceInMinutes)
+        [Arguments(30,   3, 0, 8)]
+        [Arguments(60,   3, 0, 8)]
+        [Arguments(120,  3, 0, 8)]
+        [Arguments(180,  3, 0, 8)]
+        [Arguments(240,  3, 0, 8)]
+        [Arguments(360,  3, 0, 8)]
+        [Arguments(720,  3, 0, 8)]
+        [Arguments(1440, 3, 0, 8)]
+
+        [Arguments(30,   15, 0, 8)]
+        [Arguments(60,   15, 0, 8)]
+        [Arguments(120,  15, 0, 8)]
+        [Arguments(180,  15, 0, 8)]
+        [Arguments(240,  15, 0, 8)]
+        [Arguments(360,  15, 0, 8)]
+        [Arguments(720,  15, 0, 8)]
+        [Arguments(1440, 15, 0, 8)]
+
+        [Arguments(30,   30, 0, 8)]
+        [Arguments(60,   30, 0, 8)]
+        [Arguments(120,  30, 0, 8)]
+        [Arguments(180,  30, 0, 8)]
+        [Arguments(240,  30, 0, 8)]
+        [Arguments(360,  30, 0, 8)]
+        [Arguments(720,  30, 0, 8)]
+        [Arguments(1440, 30, 0, 8)]
+
+        [Arguments(30,   50, 0, 8)]
+        [Arguments(60,   50, 0, 8)]
+        [Arguments(120,  50, 0, 8)]
+        [Arguments(180,  50, 0, 8)]
+        [Arguments(240,  50, 0, 8)]
+        [Arguments(360,  50, 0, 8)]
+        [Arguments(720,  50, 0, 8)]
+        [Arguments(1440, 50, 0, 8)]
+
+        [Arguments(120, 3, 0, 8)]
+        [Arguments(120, 3, 0, 6)]
+        [Arguments(120, 3, 0, 4)]
+        [Arguments(120, 3, 4, 8)]
+        [Arguments(120, 3, 4, 6)]
+
+        [Arguments(120, 15, 0, 8)]
+        [Arguments(120, 15, 0, 6)]
+        [Arguments(120, 15, 0, 4)]
+        [Arguments(120, 15, 4, 8)]
+        [Arguments(120, 15, 4, 6)]
+
+        [Arguments(120, 30, 0, 8)]
+        [Arguments(120, 30, 0, 6)]
+        [Arguments(120, 30, 0, 4)]
+        [Arguments(120, 30, 4, 8)]
+        [Arguments(120, 30, 4, 6)]
+
+        [Arguments(120, 50, 0, 8)]
+        [Arguments(120, 50, 0, 6)]
+        [Arguments(120, 50, 0, 4)]
+        [Arguments(120, 50, 4, 8)]
+        [Arguments(120, 50, 4, 6)]
+        public void MultipleCriteriaDijkstra(
+            int maxTimeAheadFetchingPossibleConnections, 
+            int minimumPossibleConnectionsFetched, 
+            int minimalTransferTime, 
+            int maximalTransferDistanceInMinutes)
         {
             var list = new List<BestConnections>();
-            var parameters = CreateParameters(minimalTransferTime, maximalTransferDistanceInMinutes);
+            var parameters = CreateParameters(
+                maxTimeAheadFetchingPossibleConnections, minimumPossibleConnectionsFetched, minimalTransferTime, maximalTransferDistanceInMinutes);
             foreach (var search in Searches)
             {
                 var searchInput = searchInputMapper.ToDomainFromGraph(search, parameters, Graph);
@@ -99,12 +154,16 @@ namespace Chilicki.Ptsa.Benchmarks
             list.Consume(new Consumer());
         }
 
-        private SearchParameters CreateParameters(int minimalTransferTime, int maximalTransferDistanceInMinutes)
+        private SearchParameters CreateParameters(
+            int maxTimeAheadFetchingPossibleConnections,
+            int minimumPossibleConnectionsFetched,
+            int minimalTransferTime,
+            int maximalTransferDistanceInMinutes)
         {
             return new SearchParameters()
             {
-                MaxTimeAheadFetchingPossibleConnections = MaxTimeAheadFetchingPossibleConnections,
-                MinimumPossibleConnectionsFetched = MinimumPossibleConnectionsFetched,
+                MaxTimeAheadFetchingPossibleConnections = maxTimeAheadFetchingPossibleConnections,
+                MinimumPossibleConnectionsFetched = minimumPossibleConnectionsFetched,
                 MinimalTransferTime = minimalTransferTime,
                 MaximalTransferDistanceInMinutes = maximalTransferDistanceInMinutes,
             };
